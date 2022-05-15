db3062acb49241ad3586d6b76de5aa6abd035524
Tibor Micheľ <tibor.michel@student.tuke.sk>
2022-05-15 12:41:56 +0200

    aa

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -25,12 +25,12 @@ from django.views.static import serve
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
     ]
 
 urlpatterns += i18n_patterns(
-
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
     path("calculator_app/",include("calculator.urls")),
 
