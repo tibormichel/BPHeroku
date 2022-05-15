cd3d0e149a403a313e29f75edacf4fbd54002d3f
Tibor Micheľ <tibor.michel@student.tuke.sk>
2022-05-15 12:56:18 +0200

    aa

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -142,3 +143,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/person_profile_pics/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/person_profile_pics')
+
--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATICFILES_DIRS}),
 
     ]
 
