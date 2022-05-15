9668b225e3681f970230282810819aef509edb6d
Tibor Micheľ <tibor.michel@student.tuke.sk>
2022-05-15 16:33:28 +0200

    q

--- projects/models.py
+++ projects/models.py
@@ -5,30 +5,30 @@ from people.models import Person
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(null=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True, blank=True)
     title_sk = models.CharField(max_length=255, blank=True, null=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     # category_name = category.title
 
-    created_by = models.ForeignKey(Person, on_delete=models.CASCADE, default=1)
+    created_by = models.ForeignKey(Person, on_delete=models.CASCADE, default=1, null=True, blank=True)
     # related_publication = models.OneToOneField('publications.Publication', on_delete=models.CASCADE, null=True)
     # related_publication = models.ManyToManyField('publications.Publication', related_name='publication')
     # related_publication = models.ManyToManyRel('publications.Publication', related_name='publication')
     # related_publication = models.ManyToManyField('publications.Publication')
-    # related_publication = models.ForeignKey('publications.Publication', on_delete=models.CASCADE, null=True)            TOTO A RIADOK POD TYM
-    # related_research = models.ForeignKey('research.Research', on_delete=models.CASCADE, null=True)
+    related_publication = models.ForeignKey('publications.Publication', on_delete=models.CASCADE, null=True, blank=True)
+    related_research = models.ForeignKey('research.Research', on_delete=models.CASCADE, null=True, blank=True)
     # related_research = models.OneToOneField('research.Research', on_delete=models.CASCADE, null=True)
-    body = RichTextField(blank=True)
+    body = RichTextField(blank=True, null=True)
     body_sk = RichTextField(blank=True, null=True)
     # def __str__(self):
     #     template = self.title + ' | ' + str(self.author)
