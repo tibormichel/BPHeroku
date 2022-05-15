93bdb9f76b97a6f5465bfe4c8b0aa1b1088ca2ee
Tibor Micheľ <tibor.michel@student.tuke.sk>
2022-05-15 15:59:09 +0200

    q

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -132,7 +132,7 @@ LOCALE_PATHS = [
 ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
