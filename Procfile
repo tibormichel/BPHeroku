50cf28c3d555e9a7c47a37bd799bccb39c0bc0d9
Tibor Micheľ <tibor.michel@student.tuke.sk>
2022-05-15 15:49:42 +0200

    q

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djangoProject2.wsgi
--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'djangoProject2.urls'
@@ -134,6 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -141,3 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/person_profile_pics/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/person_profile_pics')
+
+
+django_heroku.settings(locals())
