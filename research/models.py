33c6e44c2a632febe0ee5ddbaf53f9d41d0e8ef9
Tibor Micheľ <tibor.michel@student.tuke.sk>
2022-05-16 08:33:02 +0200

    q

--- research/models.py
+++ research/models.py
@@ -11,8 +11,8 @@ class Research(models.Model):
     title_sk = models.CharField(max_length=255, blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_by = models.ForeignKey(Person, on_delete=models.CASCADE, default=1)
-    related_publication = models.OneToOneField('publications.Publication', on_delete=models.CASCADE, null=True)
-    related_project = models.OneToOneField('projects.Project', on_delete=models.CASCADE, null=True)
+    related_publication = models.OneToOneField('publications.Publication', on_delete=models.CASCADE, null=True, blank=True)
+    related_project = models.OneToOneField('projects.Project', on_delete=models.CASCADE, null=True, blank=True)
     body = RichTextField(blank=True)
     body_sk = RichTextField(blank=True, null=True)
 
